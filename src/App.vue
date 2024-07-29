9799eb635101513115c6caabaee0ba68d382d7ff
Thabiso <tmosenohi99@gmail.com>
2024-07-29 12:43:02 +0200

    Implimented component cards

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import q from './data/quizes.json';
 import { ref,watch } from 'vue';
+import Card from './Card.vue';
 
 const quizes = ref(q);
 const search = ref('');
@@ -18,13 +19,17 @@ watch(search, ()=>{
     </header>
     
     <div class="options-container">
-      <div class="card" v-for="quiz in quizes" :key="quiz.id">
+      <!-- <div class="card" v-for="quiz in quizes" :key="quiz.id">
         <img :src="quiz.img" alt="">
         <div class="card-text">
           <h2>{{quiz.name}}</h2>
           <p>{{ quiz.questions.length }} questions</p>
         </div>
-      </div>
+      </div> -->
+      <Card v-for="quiz in quizes" :key="quiz.id" :quiz="quiz">
+
+      </Card>
+
     </div>
   </div>
 </template>
@@ -62,27 +67,5 @@ watch(search, ()=>{
 
   /* CARD */
 
-  .card {
-    width: 310px;
-    overflow: hidden;
-    border-radius: 2%;
-    box-shadow: 1px 1px 10px rgba(0,0,0,0.1);
-    margin-bottom: 35px;
-    margin-right: 20px;
-    cursor: pointer;
-  }
-
-  .card img {
-    width: 100%;
-    height: 190px;
-    margin: 0
-  }
-
-  .card .card-text {
-    padding: 0 5px
-  }
-
-  .card .card-text h2 {
-    font-weight: bold;
-  }
+ 
 </style>
