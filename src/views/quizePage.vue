ab7d32497ef94b7230cc161e0966d9f01fc645d9
Thabiso <tmosenohi99@gmail.com>
2024-08-05 12:47:26 +0200

    Question status updates

--- src/views/quizePage.vue
+++ src/views/quizePage.vue
@@ -2,17 +2,22 @@
 import question from '../components/question.vue';
 import quizHeader from '../components/quizHeader.vue';
 import { useRoute } from 'vue-router';
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
 import quizes from '../data/quizes.json';
 
+//get the quiz ID
 const route = useRoute();
 const quizID = parseInt(route.params.id);
 
+//assign current quiz to the quiz in the fetched list ie, the one from the json
 const quiz=quizes.find(q=>q.id === quizID)
 
 const currentQuestionIndex = ref(0);
-const questionStatus =`${currentQuestionIndex.value+1}/${quiz.questions.length}`
+const questionStatus =ref(`${currentQuestionIndex.value}/${quiz.questions.length}`)
 
+watch(()=>currentQuestionIndex.value, ()=>{
+    questionStatus.value = `${currentQuestionIndex.value}/${quiz.questions.length}`;
+})
 </script>
 
 <template>
@@ -25,6 +30,7 @@ const questionStatus =`${currentQuestionIndex.value+1}/${quiz.questions.length}`
         <div>
        
         </div>
+        <button @click="currentQuestionIndex++">Next question</button>
     </div>
 </template>
 
